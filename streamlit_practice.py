41d11e1b1382a839fdad802928675b99257fe8ea
Kirtan Tank <89449817+Kirtan-Tank@users.noreply.github.com>
2023-11-09 16:25:28 +0530

    Update streamlit_practice.py

--- streamlit_practice.py
+++ streamlit_practice.py
@@ -1,4 +1,11 @@
 import streamlit as st
 
 st.header("STREAMLIT PRACTICE")
-st.header('st.button')
+
+#Button
+st.header('-> st.button')
+
+if st.button('Click button'):
+  st.write("Button Pressed")
+else:
+  st.write("Button Not yet pressed")
