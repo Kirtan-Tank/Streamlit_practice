cb556da107cdc8477c266ad5b748b924c995938f
Kirtan Tank <89449817+Kirtan-Tank@users.noreply.github.com>
2023-11-09 17:09:46 +0530

    Update streamlit_practice.py

--- streamlit_practice.py
+++ streamlit_practice.py
@@ -13,4 +13,6 @@ else:
 st.subheader("This is a subheader")
 st.text("This is text cannot change font")
 
-st.markdown("- # _This is italic h1 markdown heading_ - ## _This is italic h2 markdown heading_ - ### _This is italic h3 markdown heading_")
+st.markdown(""" # _This is italic h1 markdown heading_ 
+- ## _This is italic h2 markdown heading_ 
+- ### _This is italic h3 markdown heading_ """)
